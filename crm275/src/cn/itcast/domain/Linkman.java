014559783e0a42bc7aeac1925e9b382540d728dd
utopian <403584467@qq.com>
2017-12-28 18:49:44 +0800

    11

--- crm275/src/cn/itcast/domain/Linkman.java
+++ crm275/src/cn/itcast/domain/Linkman.java
@@ -13,7 +13,7 @@ import javax.persistence.Table;
 @Table(name="cst_linkman")
 public class Linkman {
 
-	private String aa;
+	private String a1;
 	@Id
 	@GeneratedValue(strategy=GenerationType.IDENTITY)
 	@Column(name="lkm_id")
