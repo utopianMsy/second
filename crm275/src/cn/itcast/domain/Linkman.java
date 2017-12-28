e4b6dd2884b2b22d6163c8cc245adf1760147845
utopian <403584467@qq.com>
2017-12-28 18:51:15 +0800

    将a1变成bb

--- crm275/src/cn/itcast/domain/Linkman.java
+++ crm275/src/cn/itcast/domain/Linkman.java
@@ -13,7 +13,7 @@ import javax.persistence.Table;
 @Table(name="cst_linkman")
 public class Linkman {
 
-	private String a1;
+	private String bb;
 	@Id
 	@GeneratedValue(strategy=GenerationType.IDENTITY)
 	@Column(name="lkm_id")
